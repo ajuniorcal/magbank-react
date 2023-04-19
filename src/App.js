ef02c886b06126ac6db509357010f768ec495038
Antonio Junior <ajr3090@gmail.com>
2023-04-19 00:34:39 -0300

    Continuacao hero

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import Navbar from './compontents/Navbar';
-import './App.scss';
+import "./App.css";
 
+import Navigation from "./compontents/Navigation";
+import Hero from "./compontents/Hero";
+import CenteredButton from "./compontents/CenteredButton";
+import CardList from "./compontents/CardList";
+import CreditCard from "./compontents/CreditCard";
 
+import posts from "./data/posts";
 
-const App = () =>  (
+function App() {
+  return (
     <div className="App">
-      <Navbar/>
+      <Navigation />
+      <Hero />
+      <CreditCard />
+      <CardList posts={posts} />
+      <CenteredButton>Abra a sua conta</CenteredButton>
     </div>
   );
-
+}
 
 export default App;
---
+++ src/compontents/IconText.js
@@ -0,0 +1,17 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const IconText = ({ icon, children }) => {
+  return (
+    <Row>
+      <Col lg={2}>
+        <FontAwesomeIcon icon={icon} />
+      </Col>
+
+      <Col>{children}</Col>
+    </Row>
+  );
+};
+
+export default IconText;
